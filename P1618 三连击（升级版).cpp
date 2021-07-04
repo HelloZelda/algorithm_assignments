8f8a4d7cb260c9ad21dafa871a4752212ba9ad7c
HelloZelda <85979022+HelloZelda@users.noreply.github.com>
2021-07-04 22:28:06 +0800

    徐畅-本地已测试-洛谷100分

---
+++ P1618 三连击（升级版).cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[10]={0,1,2,3,4,5,6,7,8,9};
+int main()
+{
+    int A,B,C,ok=0;
+    cin>>A>>B>>C;
+    int t=A*B*C;
+    A=t/A;
+    B=t/B;
+    C=t/C;
+    do
+    {
+        if((100*a[1]+10*a[2]+a[3])*A==(100*a[4]+10*a[5]+a[6])*B&&(100*a[1]+10*a[2]+a[3])*A==(100*a[7]+10*a[8]+a[9])*C)
+        {
+            cout<<a[1]<<a[2]<<a[3]<<" "<<a[4]<<a[5]<<a[6]<<" "<<a[7]<<a[8]<<a[9]<<endl;
+            ok++;
+        }
+    }
+    while(next_permutation(a+1,a+10));
+    if(ok==0) 
+    {
+        cout<<"No!!!";
+    }
+    return 0;
+}
